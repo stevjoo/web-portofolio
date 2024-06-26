811cd145675807af8593940456b94f9aee016de1
stevjoo <goodjestcom@gmail.com>
2024-06-26 14:05:28 +0700

    tambahin animasi photo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="flex flex-col lg:flex-row h-screen">
       {/* Text and Icons Section */}
       <div className="lg:w-3/5 flex flex-col justify-center py-8 px-8">
-        <div className="flex flex-col text-center lg:text-right">
+        <div className="flex flex-col text-left mb-2 lg:text-right lg:mb-0">
           <motion.div
             className="text-3xl mt-20 lg:text-4xl font-bold text-yellow-400 mb-2"
             initial={{ opacity: 0, scale: 1 }}
@@ -40,8 +40,7 @@ function App() {
           >
             <p className="text-gray-400 mb-3">
               <span className='transition-color ease-in-out duration-700 hover:text-white'>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              </span>
+                Hi, I'm a Computer Science student at Universitas Multimedia Nusantara. I love learning new things and building my skills. When I'm not hitting the books, you can find me with a camera, capturing moments that tell stories, or mixing audio to create immersive soundscapes. I'm also a Software Engineer and currently learning more about Cybersecurity. Always exploring new interests and trying to be better at what I do.</span>
             </p>
           </motion.div>
         </div>
@@ -50,7 +49,7 @@ function App() {
           <div className="flex space-x-4">
             <a target='_blank' href='#'>
               <motion.div
-                initial={{ x: "-200%", opacity: 0, rotate: -140}}
+                initial={{ x: "-200%", opacity: 0, rotate: -140 }}
                 animate={{ x: 0, opacity: 1, rotate: 0 }}
                 exit={{ x: "100%" }}
                 transition={{
@@ -75,13 +74,13 @@ function App() {
               </motion.div>
             </a>
 
-            <a target='_blank' href='#'>
+            <a target='_blank' href='https://www.instagram.com/stev.jo__?igsh=eWdyZTQ2ZTAxcngz'>
               <motion.div
-                initial={{ x: "-300%", opacity: 0, rotate:-140}}
+                initial={{ x: "-300%", opacity: 0, rotate: -140 }}
                 animate={{ x: 0, opacity: 1, rotate: 0 }}
                 exit={{ x: "100%" }}
                 transition={{
-                  duration: 4,
+                  duration: 5,
                   type: "spring",
                   damping: 20,
                   stiffness: 20,
@@ -104,11 +103,11 @@ function App() {
 
             <a href='#'>
               <motion.div
-                initial={{ x: "-400%", opacity: 0, rotate: -140}}
-                animate={{ x: 0, opacity: 1, rotate: 0}}
+                initial={{ x: "-400%", opacity: 0, rotate: -140 }}
+                animate={{ x: 0, opacity: 1, rotate: 0 }}
                 exit={{ x: "100%" }}
                 transition={{
-                  duration: 4,
+                  duration: 6,
                   type: "spring",
                   damping: 20,
                   stiffness: 20,
@@ -131,11 +130,11 @@ function App() {
 
             <a href='#'>
               <motion.div
-                initial={{ x: "-500%", opacity: 0, rotate: -140}}
-                animate={{ x: 0, opacity: 1, rotate: 0}}
+                initial={{ x: "-500%", opacity: 0, rotate: -140 }}
+                animate={{ x: 0, opacity: 1, rotate: 0 }}
                 exit={{ x: "100%" }}
                 transition={{
-                  duration: 4,
+                  duration: 7,
                   type: "spring",
                   damping: 20,
                   stiffness: 20,
@@ -160,7 +159,7 @@ function App() {
 
           <a target='_blank' href='https://wa.me/+628111396967'>
             <motion.div
-              initial={{ y: "50%" , opacity: 0 }}
+              initial={{ y: "50%", opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               exit={{ x: "100%" }}
               transition={{
@@ -172,21 +171,48 @@ function App() {
               }}
             >
               <motion.button
-                whileHover={{ scale: 1.1 }}
+                whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.9 }}
                 transition={{ duration: 0.5 }}
-                className="mt-2 px-4 py-2 bg-yellow-400 rounded-lg hover:scale-110 hover:bg-white font-bold"
+                className="mt-2 px-4 py-2 bg-yellow-400 rounded-lg hover:scale-110 text-black hover:bg-white font-bold flex items-center"
                 onClick={() => null}
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="ml-0 mr-3"
+                >
+                  <path
+                    d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"
+                  />
+                </svg>
                 Contact Me
               </motion.button>
             </motion.div>
           </a>
+
         </div>
       </div>
       {/* Image Section */}
-      <div className="lg:w-2/5 flex justify-center lg:justify-end items-end grayscale">
-        <img src={foto} alt="Steven Jonathan" className="ngefadefoto object-cover h-full w-full" />
+
+      <div className="lg:w-2/5 flex justify-center lg:justify-end items-end brightness-200 grayscale hover:grayscale-0 transition ease-in-out duration-500 ">
+        <motion.div
+          className='object-cover h-full w-full'
+          initial={{ x: "0%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: "100%" }}
+          transition={{
+            duration: 4,
+            type: "spring",
+            damping: 20,
+            stiffness: 20,
+            ease: "0, 0.5, 0.3, 0.8",
+          }}
+        >
+          <img src={foto} alt="Steven Jonathan" className="ngefadefoto" />
+        </motion.div>
       </div>
     </div>
   );
